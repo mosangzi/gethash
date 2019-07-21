e1c53cea3e0e6e768bfc3ffa0a144dbb3b8feef6
mosangzi <zhangxiaokaihit@gmail.com>
2019-07-21 22:37:41 +0800

    Add files via upload

---
+++ gethash.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+import hashlib,sys
+sha256=hashlib.sha256()
+with open(sys.argv[1],'rb') as fr:
+    for each in fr:
+        sha256.update(each)
+hash_value=sha256.hexdigest()
+print(hash_value)
